e85c85234e7d97f97ca98bc0843e1f9890a02682
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-03 13:48:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+let url = 'http://localhost:3000/users';
+function submitData (userName, userEmail) {
+  const body = document.getElementsByTagName('body')[0];
+    let person = {
+      name: userName,
+      email: userEmail
+    };
+    let configObj = {
+
+
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(person),
+    method : 'POST'
+  };
+
+  return fetch(url ,  configObj)
+.then(response=> response.json())
+.then(
+user => {
+  body.innerHTML =  user.id;}
+  /* end of second then*/)
+  .catch(
+    err => {
+       body.innerHTML =  err.message;
+    }
+  )
+
+
+/* end of function*/}
