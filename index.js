ea16e2c4b48f95a0b50b7e7b813d90e61326b768
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-03 17:19:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Add your code here
+<<<<<<< HEAD
 let url = 'http://localhost:3000/users';
+=======
+>>>>>>> f0885158585dd01bb732977a0bbe40836727fa25
 function submitData (userName, userEmail) {
   const body = document.getElementsByTagName('body')[0];
     let person = {
@@ -17,7 +20,11 @@ function submitData (userName, userEmail) {
     method : 'POST'
   };
 
+<<<<<<< HEAD
   return fetch(url ,  configObj)
+=======
+  return fetch('http://localhost:3000/users',  configObj)
+>>>>>>> f0885158585dd01bb732977a0bbe40836727fa25
 .then(response=> response.json())
 .then(
 user => {
